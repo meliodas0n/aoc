89dd883a7d8dedfa7d02af6f8d33a645a968511a
meliodas0n <mrunalnarayana@gmail.com>
2025-01-01 07:31:09 +0000

    Day 2 Puzzle 1, code is stupid tho

--- AOC_2024/2/1.py
+++ AOC_2024/2/1.py
@@ -2,9 +2,38 @@ import os, sys
 sys.path.append(os.path.abspath("../"))
 
 import utils
+from itertools import pairwise 
 
 def main():
-    inputs = utils.read_inputs("1.input")
+    unusal_data = utils.read_inputs("1.input")
+    safe_rep_count = 0
+    for report in unusal_data:
+        rep = [int(x) for x in report.split()]
+        flag = 0
+        if rep == sorted(rep):
+            flag = 1
+        elif rep == sorted(rep, reverse=True):
+            flag = -1
+        else:
+            continue
+        diff_flag = 0
+        if flag == -1:
+            for prev_ele, next_ele in pairwise(rep):
+                if ((prev_ele - next_ele) >= 1) and ((prev_ele - next_ele) <= 3):
+                    diff_flag = 1
+                else:
+                    diff_flag = 0
+                    break
+        if flag == 1:
+            for prev_ele, next_ele in pairwise(rep):
+                if ((next_ele - prev_ele) >= 1) and ((next_ele - prev_ele) <= 3):
+                    diff_flag = 1
+                else:
+                    diff_flag = 0
+                    break
+        if diff_flag == 1:
+            safe_rep_count += 1
+    print(f"{safe_rep_count = }")
     
 if __name__ == "__main__":
     main()
